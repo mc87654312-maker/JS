45d2fc178948712868016745a3076f32462dd8d5
mc87654312-maker <mc87654312@gmail.com>
2026-07-09 06:21:56 +0000

    Date and Time

---
+++ .devcontainer/01_basics/07_datesinJs.js
@@ -0,0 +1,46 @@
+//DATES
+
+// let myDate = new Date()
+// console.log(myDate.toString());
+
+// console.log(myDate.toJSON());
+
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+
+// console.log(typeof myDate);
+
+
+// let myCreateDate = new Date(2025, 1, 24);
+// console.log(myCreateDate.toDateString());
+
+
+// let myCreateDate = new Date("2025, 01, 24");
+// console.log(myCreateDate.toDateString());
+
+//let myCreateDate = new Date("01-19-2025");
+//console.log(myCreateDate.toDateString());
+
+// let myTimeStamp = Date.now();
+
+// console.log(myTimeStamp);
+// console.log(myCreateDate.getTime);
+// console.log(Math.floor(Date.now()/1000));
+
+
+let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth());
+console.log(newDate.getUTCFullYear);
+
+
+newDate.toLocaleString('default', {
+    weekday: "long",
+    
+})
+
+
+
+
+
+
