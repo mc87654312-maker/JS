c83bd8864f216b38a2947397d0ddc1941b189fe1
mc87654312-maker <mc87654312@gmail.com>
2026-07-12 06:13:32 +0000

    global and local scope

--- .devcontainer/03_basics/02_scopes.js
+++ .devcontainer/03_basics/02_scopes.js
@@ -8,10 +8,23 @@ if (true) {
 
     let a = 10
     const b = 20
-    var c = 30
+    
+    console.log("INNER: ", a);
 }
 
 
-//console.log(a);
-//console.log(b);
-console.log(c);
+// console.log(a);
+// console.log(b);
+//console.log(c);
+
+function one(){
+    const username = "Madhab"
+    
+    function two(){
+        const website = "Youtube"
+        console.log(username);
+    }
+    //console.log(website);
+    two()
+}
+one()
